0dc3fbfb75325c010f23b976389695bdbfd9049e
Jason Hessels <hessels@astron.nl>
2012-02-08 12:56:11 +0000

    Skeleton version of the main pipeline script

--- python/epta_timing_pipeline.py
+++ python/epta_timing_pipeline.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-####################
-# epta_pipeline.py #
+##################################
+# epta_timing_pipeline.py 
 PIPE_NAME = "epta_timing_pipeline"
 VERSION = 0.2
-####################
+##################################
 
 #Imported modules
 from sys import argv, exit
@@ -22,7 +22,7 @@ import epta_pipeline_utils as epta
 DB_HOST = "localhost"
 DB_NAME = "epta"
 DB_USER = "epta"
-DB_PASS = "mysqlaccess"
+DB_PASS = "psr1937"
 
 #Python version to use
 PYTHON = "/usr/bin/python"
@@ -44,7 +44,7 @@ def Help():
     print "\nThe EPTA Timing Pipeline"
     print "Version: %.2f"%(VERSION)+"\n"
         
-    print "Please use 'epta_pipeline.py -h' for a full list of command line options. \n"
+    print "Please use 'epta_timing_pipeline.py -h' for a full list of command line options. \n"
 
     print "\n"
     exit(0)
@@ -53,74 +53,97 @@ def Parse_command_line():
     parser = argparse.ArgumentParser(
         prog='epta_pipeline',
         description='')
-    #Standard profile
-    parser.add_argument('--std_prof_id',
+    #Raw data
+    parser.add_argument('--rawfile_id',
                         nargs=1,
                         type=int,
                         default=None,
-                        help="psrfits_id of standard profile to use for running the full pipeline.")
-    #Do a full pipeline run
-    parser.add_argument('--full_pipeline',
+                        help="ID of raw data file to use for running the full pipeline.")
+    #Ephemeris
+    parser.add_argument('--ephem_id',
                         nargs=1,
-                        type=str,
+                        type=int,
                         default=None,
-                        help="Run the full pipeline.")
+                        help="ID of ephemeris to use for running the full pipeline.")
+    #Template profile
+    parser.add_argument('--template_id',
+                        nargs=1,
+                        type=int,
+                        default=None,
+                        help="ID of template profile to use for running the full pipeline.")
     args=parser.parse_args()
     return args
 
 
 def main():
 
+    #Exit if there are no or insufficient arguments
     if len(argv) < 2:
         Help()
 
     args = Parse_command_line()
 
-    if args.full_pipeline and args.std_prof_id:
-        print "###################################################"
-        print "Starting EPTA Timing Pipeline Version %.2f"%VERSION
-        proc_id = Make_Proc_ID()
-        print "Proc ID (UTC start datetime): %s"%proc_id
-        print "Start time: %s"%Give_UTC_now()
-        print "###################################################"
-
-        file = args.full_pipeline[0]
-        std_prof_id = args.std_prof_id[0]
-        file_path, file_name = Verify_file_path(file, verbose=VERBOSE)
-        print "Running on %s"%os.path.join(file_path, file_name)
-        Run_EPTA_pipeline(file,proc_id,std_prof_id)
-
-        print "###################################################"
-        print "Finished EPTA Timing Pipeline Version %.2f"%VERSION
-        print "End time: %s"%Give_UTC_now()
-        print "###################################################"
-    elif args.std_prof:
-        std_prof = args.std_prof[0]
-        print "########################################################"
-        print "Uploading a standard profile %s to the EPTA DB"%std_prof
-        proc_id = Make_Proc_ID()
-        print "Proc ID (UTC start datetime): %s"%proc_id
-        print "Start time: %s"%Give_UTC_now()
-        print "########################################################"
-        
-        #Make DB connection
-        DBcursor, DBconn = DBconnect(DB_HOST,DB_NAME,DB_USER,DB_PASS)
-
-        #Fill pipeline table
-        pipeline_id = Fill_pipeline_table(DBcursor,DBconn)
-
-        psrfits_id, par_id = DB_injest_psrfits(std_prof,'std_prof',proc_id,DBcursor,DBconn,verbose=VERBOSE)
-
-        #Close DB connection
-        DBconn.close()
-
-        print "########################################################"
-        print "Successfully uploaded standard profile %s to the EPTA DB"%std_prof
-        print "This standard profile has psrfits_id = %d"%psrfits_id
-        print "End time: %s"%Give_UTC_now()
-        print "########################################################"
-    else:
+    if not (args.rawfile_id and args.ephem_id and args.template_id):
         print "\nYou haven't specified a valid set of command line options.  Exiting..."
         Help()
-        
-main()
+
+    rawfile_id = args.rawfile_id[0]
+
+    #Start pipeline
+    print "###################################################"
+    print "Starting EPTA Timing Pipeline Version %.2f"%VERSION
+    proc_id = epta.Make_Proc_ID()
+    print "Proc ID (UTC start datetime): %s"%proc_id
+    print "Start time: %s"%epta.Give_UTC_now()
+    print "###################################################"
+
+    #Make DB connection
+    DBcursor, DBconn = epta.DBconnect(DB_HOST,DB_NAME,DB_USER,DB_PASS)
+
+    DBcursor.execute("show tables")
+    print DBcursor.fetchall()
+
+    #Fill pipeline table
+    #pipeline_id = epta.Fill_pipeline_table(DBcursor,DBconn)
+
+    #Get raw data from rawfile_id and verify MD5SUM
+    #raw_file = epta.get_raw_data()
+    print rawfile_id
+    DBcursor.execute("select filename, filepath, md5sum from rawfiles where rawfile_id = %d"%rawfile_id)
+    filename, filepath, md5sum = DBcursor.fetchall()
+    rawfile = os.path.join(filepath,filename)
+    epta.Verify_file_path(rawfile)
+    epta.Verify_MD5SUM(md5sum,rawfile)
+    
+    
+    #Get ephemeris from ephem_id and verify MD5SUM
+    #ephemeris = epta.get_ephem()
+
+    #Reinstall ephemeris with pam
+
+    #Scrunch data in time/freq
+
+    #Make diagnostic plots of scrunched data
+
+    #Get template from template_id and verify MD5SUM
+
+    #Generate TOA with pat
+
+    #Make plots associated with the TOA generation
+
+    #Insert TOA into DB
+
+    #Close DB connection
+    print "Closing DB connection..."
+    DBconn.close()
+
+    #End pipeline
+    print "###################################################"
+    print "Finished EPTA Timing Pipeline Version %.2f"%VERSION
+    print "End time: %s"%epta.Give_UTC_now()
+    print "###################################################"
+
+if __name__ == "__main__":
+    main()
+
+
