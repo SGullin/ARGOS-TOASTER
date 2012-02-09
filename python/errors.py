e1b1b67df89acf02bf179f141206dd980ba67200
Patrick Lazarus <plazar@mpifr-bonn.mpg.de>
2012-02-09 12:53:21 +0000

    Add custom errors classes.

---
+++ python/errors.py
@@ -0,0 +1,12 @@
+"""
+This file contains custom errors for the EPTA timing pipeline.
+
+Patrick Lazarus, Feb. 9, 2012
+"""
+
+class EptaPipelineError(Exception):
+    pass
+
+
+class SystemCallError(EptaPipelineError):
+    pass
