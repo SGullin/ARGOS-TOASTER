293ae8d60735e0e8a6cd987534ecc8ec09e56f94
Patrick Lazarus <plazar@mpifr-bonn.mpg.de>
2012-05-05 15:30:40 +0100

    Add support for transactioning database queries.

--- python/database.py
+++ python/database.py
@@ -12,15 +12,17 @@ class Database(object):
     """Database object for connecting to the EPTA database
         using MySQLdb
     """
-    def __init__(self):
+    def __init__(self, autocommit=True):
         """Constructor for Database object.
 
             Inputs:
-                None
+                autocommit: If True, automatically commit each query as 
+                    it is executed. (Default: True)
 
             Output:
                 db: connected Database object.
         """
+        self.autocommit=autocommit
         self.connect()
 
     def connect(self):
@@ -36,7 +38,8 @@ class Database(object):
             self.conn = MySQLdb.connect(host=config.dbhost, \
                                         db=config.dbname, \
                                         user=config.dbuser, \
-                                        passwd=config.dbpass)
+                                        passwd=config.dbpass, \
+                                        autocommit=False)
             self.cursor = self.conn.cursor(MySQLdb.cursors.Cursor)
             epu.print_debug("Successfully connected to database %s.%s as %s" % \
                         (config.dbhost, config.dbname, config.dbuser), \
@@ -52,6 +55,8 @@ class Database(object):
             msg += "\nKeyword args: %s" % kwargs
         epu.print_debug(msg, 'database')
         self.cursor.execute(query, *args, **kwargs)
+        if self.autocommit:
+            self.commit()
         try:
             colnames = [d[0] for d in self.cursor.description]
             self.RowClass = collections.namedtuple("RowClass", colnames)
@@ -60,6 +65,31 @@ class Database(object):
             # If we run into a problem default to a tuple
             self.RowClass = tuple
             self.row_maker = self.RowClass
+
+    def commit(self):
+        """Commit a transaction.
+            
+            Inputs:
+                None
+
+            Outputs:
+                None
+        """
+        if not self.autocommit:
+            epu.print_debug("Manually committing transaction.", 'database')
+        self.conn.commit()
+
+    def rollback(self):
+        """Rollback an uncommitted a transaction.
+            
+            Inputs:
+                None
+
+            Outputs:
+                None
+        """
+        epu.print_debug("Rolling back transaction.", 'database')
+        self.conn.rollback()
 
     def close(self):
         """Close the DB connection.
