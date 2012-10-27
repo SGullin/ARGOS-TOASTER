37eec7e0ddbdfc5e985aefaea9e5756cd5119ff3
Patrick Lazarus <plazarus@mpifr.de>
2012-10-27 14:19:35 +0200

    Add default cmdline options to 'create_tables.py'.

--- python/create_tables.py
+++ python/create_tables.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 import database
+import utils
 
-db = database.Database()
-db.metadata.create_all(db.engine)
+def main():
+    db = database.Database()
+    db.metadata.create_all(db.engine)
+
+if __name__=='__main__':
+    parser = utils.DefaultArguments(\
+                description="Create TOASTER database tables.")
+    args = parser.parse_args()
+    main()
