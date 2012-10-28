3c065240b8995ae71e8d17ef2037fdd8f2b42f59
Patrick Lazarus <plazarus@mpifr.de>
2012-10-28 11:33:44 +0100

    List of pulsars can be limited in 'show_pulsars.py'.

--- python/show_pulsars.py
+++ python/show_pulsars.py
@@ -15,8 +15,12 @@ def main():
     # Build caches
     utils.get_pulsarname_cache()
     pulsaralias_cache = utils.get_pulsaralias_cache()
+    pulsar_ids = args.pulsar_ids + \
+                    [utils.get_pulsarid(psr) for psr in args.psrnames]
+    if not pulsar_ids:
+        pulsar_ids = pulsaralias_cache.keys()
     print "--"*25
-    for id in sorted(pulsaralias_cache.keys()):
+    for id in sorted(pulsar_ids):
         psrname = utils.get_pulsarname(id)
         print colour.cstring("Pulsar ID:", underline=True, bold=True) + \
                 colour.cstring(" %d" % id, bold=True)
@@ -33,5 +37,11 @@ def main():
 if __name__=='__main__':
     parser = utils.DefaultArguments(description="Get a listing of pulsars " \
                                         "from the DB to help the user.")
+    parser.add_argument('-p', '--psr', dest='psrnames', \
+                        type=str, default=[], action='append', \
+                        help="The pulsar to grab info for.")
+    parser.add_argument('--pulsar-id', dest='pulsar_ids', \
+                        type=int, default=[], action='append', \
+                        help="IDs of pulsars to grab info for.")
     args = parser.parse_args()
     main()
