95f14d30b697f9054a532eb44e0ea96dda90a173
Patrick Lazarus <patricklazarus@gmail.com>
2012-11-08 12:05:25 +0100

    Add 'show_obssystems.py' to list available observing systems.

---
+++ python/show_obssystems.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+This TOASTER utility script provides the user with a listing
+of observing systems in the database.
+
+Patrick Lazarus, Nov 6, 2012.
+"""
+import errors
+import colour
+import utils
+
+
+def main():
+    # Build cache
+    obssysinfo_cache = utils.get_obssysinfo_cache()
+
+    obssys_ids = obssysinfo_cache.keys()
+
+    print "--"*25
+    for id in sorted(obssys_ids):
+        obssysinfo = utils.get_obssysinfo(id)
+        print colour.cstring("Obsering System ID:", underline=True, bold=True) + \
+                colour.cstring(" %d" % id, bold=True)
+        print "Observing System Name: %s" % obssysinfo['name']
+        print "Telescope: %d" % obssysinfo['telescope_id']
+        print "Receiver: %s" % obssysinfo['frontend']
+        print "Backend: %s" % obssysinfo['backend']
+        print "Observing Band: %s" % obssysinfo['band_descriptor']
+        print "--"*25
+
+
+if __name__=='__main__':
+    parser = utils.DefaultArguments(description="Get a listing of observing " \
+                                    "systems from the DB to help the user.")
+    args = parser.parse_args()
+    main()
