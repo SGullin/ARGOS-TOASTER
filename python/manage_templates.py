04a5d039307ed3ae4e8e4bbbf8e746de5c6f9ce8
Patrick Lazarus <patricklazarus@gmail.com>
2012-12-08 10:29:34 -0500

    Add a multi-function script to manage templates.
    The script can currently be used to set master templates, remove unused templates, and query the DB for template information.

---
+++ python/manage_templates.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+manage_templates.py
+
+A multi-purpose program to manage templates.
+
+Patrick Lazarus, Dec 5, 2012
+"""
+
+import utils
+import database
+import errors
+
+toolkit = ['set_master_template', 'get_template_id', 'remove_template']
+
+
+def main():
+    args.func(args)
+
+
+if __name__ == '__main__':
+    parser = utils.DefaultArguments(prog='manage_templates.py', \
+                            description='A multi-purpose program for managing ' \
+                                'templates.')
+    subparsers = parser.add_subparsers(help='Available functionality. ' \
+                            'To get more detailed help for each function ' \
+                            'provide the "-h/--help" argument following the ' \
+                            'function.')
+    for tool_name in toolkit:
+        tool = __import__(tool_name, globals())
+        toolparser = subparsers.add_parser(tool.SHORTNAME, 
+                        help=tool.DESCRIPTION)
+        toolparser.set_defaults(func=tool.main)
+        tool.add_arguments(toolparser)
+    args = parser.parse_args()
+    main()
