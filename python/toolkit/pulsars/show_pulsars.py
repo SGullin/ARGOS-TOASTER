e3afe657cc99ffd9d93d50ed3102adedc904b63a
Patrick Lazarus <patricklazarus@gmail.com>
2013-01-16 14:09:57 +0100

    Update output of 'show_pulsars.py'.

--- python/toolkit/pulsars/show_pulsars.py
+++ python/toolkit/pulsars/show_pulsars.py
@@ -11,7 +11,6 @@ import colour
 import utils
 import database
 
-import sys
 
 SHORTNAME = 'show'
 DESCRIPTION = "Get a listing of pulsars " \
@@ -35,10 +34,12 @@ def add_arguments(parser):
                         "(Default: text).")
 
 
-def get_pulsarinfo(existdb=None):
+def get_pulsarinfo(pulsar_ids=None, existdb=None):
     """Return a dictionary of info for all pulsars.
 
         Inputs:
+            pulsar_ids: A list of pulsar IDs to get info for.
+                (Default: Get info for all pulsars).
             existdb: A (optional) existing database connection object.
                 (Default: Establish a db connection)
         
@@ -77,12 +78,12 @@ def get_pulsarinfo(existdb=None):
  
         # Get parfile info
         select = db.select([db.parfiles.c.pulsar_id, \
+                            db.parfiles.c.parfile_id, \
                             db.parfiles.c.dm, \
                             (1.0/db.parfiles.c.f0).label('period'), \
                             db.parfiles.c.raj, \
                             db.parfiles.c.decj, \
-                            (db.parfiles.c.binary_model != None).\
-                                label('is_binary')], \
+                            db.parfiles.c.binary_model], \
                     from_obj=[db.master_parfiles.\
                         outerjoin(db.parfiles, \
                             onclause=db.parfiles.c.parfile_id == \
@@ -98,13 +99,10 @@ def get_pulsarinfo(existdb=None):
         result.close()
 
         # Get pulsar names
-        pulsarname_cache = utils.get_pulsarname_cache(existdb=db, \
-                                                        update=True)
-        pulsaralias_cache = utils.get_pulsaralias_cache(existdb=db, \
-                                                                update=True)
-        userinfo_cache = utils.get_userinfo_cache(existdb=db, update=True)
-        telescopeinfo_cache = utils.get_telescopeinfo_cache(existdb=db, \
-                                                                update=True)
+        pulsarname_cache = utils.get_pulsarname_cache(existdb=db)
+        pulsaralias_cache = utils.get_pulsaralias_cache(existdb=db)
+        userinfo_cache = utils.get_userinfo_cache(existdb=db)
+        telescopeinfo_cache = utils.get_telescopeinfo_cache(existdb=db)
     except:
         trans.rollback()
         raise
@@ -116,36 +114,50 @@ def get_pulsarinfo(existdb=None):
 
     psrinfo = {}
     for psrid, name in pulsarname_cache.iteritems():
-        psrinfo[name] = {'id': psrid, \
+        if pulsar_ids is not None and psrid not in pulsar_ids:
+            continue
+        psrinfo[psrid] = {'name': name, \
                          'aliases': pulsaralias_cache[psrid], \
                          'telescopes': [], \
                          'curators': [], \
                          'numobs': 0, \
                          'numtoas': 0, \
+                         'parfile_id': None, \
                          'period': None, \
                          'dm': None, \
                          'raj': 'Unknown', \
                          'decj': 'Unknown', \
-                         'is_binary': 'Unknown'}
-    print rawfile_rows
+                         'binary': None}
     for row in rawfile_rows:
-        psr = psrinfo[pulsarname_cache[row['pulsar_id']]]
+        psrid = row['pulsar_id']
+        if pulsar_ids is not None and psrid not in pulsar_ids:
+            continue
+        psr = psrinfo[psrid]
         telname = telescopeinfo_cache[row['telescop'].lower()]['telescope_name']
         psr['telescopes'].append(telname)
         psr['numobs'] += row['numobs']
-        print psr
     for row in numtoas_rows:
-        psr = psrinfo[pulsarname_cache[row['pulsar_id']]]
+        psrid = row['pulsar_id']
+        if pulsar_ids is not None and psrid not in pulsar_ids:
+            continue
+        psr = psrinfo[psrid]
         psr['numtoas'] += row['numtoas']
     for row in parfile_rows:
-        psr = psrinfo[pulsarname_cache[row['pulsar_id']]]
+        psrid = row['pulsar_id']
+        if pulsar_ids is not None and psrid not in pulsar_ids:
+            continue
+        psr = psrinfo[psrid]
+        psr['parfile_id'] = row['parfile_id']
         psr['period'] = row['period']
         psr['dm'] = row['dm']
         psr['raj'] = row['raj']
         psr['decj'] = row['decj']
-        psr['is_binary'] = row['is_binary']
+        psr['binary'] = row['binary']
     for row in curator_rows:
-        psr = psrinfo[pulsarname_cache[row['pulsar_id']]]
+        psrid = row['pulsar_id']
+        if pulsar_ids is not None and psrid not in pulsar_ids:
+            continue
+        psr = psrinfo[psrid]
         if row['user_id'] is None:
             psr['curators'] = 'Everyone'
             break
@@ -154,60 +166,89 @@ def get_pulsarinfo(existdb=None):
     return psrinfo
 
 
-def show_pulsars(pulsar_ids):
-    """Print pulsars and aliases to screen in a human-readable
-        format.
+def show_pulsars(psrinfo):
+    """Print pulsar info to screen in a human-readable format.
 
         Input:
-            pulsar_ids: list of pulsar IDs to display.
+            psrinfo: A dictionary of pulsar info dictionaries.
+                (As returned by get_pulsarinfo(...))
 
         Outputs:
             None
     """
-    # Grab the pulsar alias cache once rather than accessing it multiple times
-    pulsaralias_cache = utils.get_pulsaralias_cache()
     print "--"*25
-    for id in sorted(pulsar_ids):
-        psrname = utils.get_pulsarname(id)
+    for psrid in sorted(psrinfo.keys()):
+        psr = psrinfo[psrid]
         print colour.cstring("Pulsar ID:", underline=True, bold=True) + \
-                colour.cstring(" %d" % id, bold=True)
-        print "Pulsar Name: %s" % psrname 
+                colour.cstring(" %d" % psrid, bold=True)
+        print "Pulsar Name: %s" % psr['name'] 
         print "Aliases:"
-        for alias in pulsaralias_cache[id]:
-            if alias == psrname:
+        for alias in psr['aliases']:
+            if alias == psr['name']:
                 continue
             print "    %s" % alias
+        if psr['parfile_id'] is None:
+            print "No parfile loaded!"
+        else:
+            if psr['period'] > 1:
+                print "Period: %.3f s" % psr['period']
+            else:
+                print "Period: %.2f ms" % (1000.0*psr['period'])
+            print "DM: %.2f pc/cc" % psr['dm']
+            print "R.A. (J2000): %s" % psr['raj']
+            print "Dec. (J2000): %s" % psr['decj']
+            print "Binary model: %s" % psr['binary']
+
+        lines = ["Number of observations: %d" % psr['numobs']]
+        if psr['numobs'] > 0:
+            lines.append("Telescopes used:\n    " + \
+                            "\n    ".join(psr['telescopes']))
+        lines.append("Number of TOAs: %d" % psr['numtoas'])
+        if psr['curators'] == 'Everyone':
+            lines.append("Curators: Everyone")
+        elif psr['curators']:
+            lines.append("Curators:\n    " + \
+                            "\n    ".join(psr['curators']))
+        else:
+            lines.append("Curators: None")
+        utils.print_info("\n".join(lines), 1)
         print "--"*25
 
 
-def dump_pulsars(pulsar_ids):
+def dump_pulsars(pulsar_ids=None):
     """Dump pulsar names and aliases to screen.
 
         Input:
             pulsar_ids: list of pulsar IDs to display.
+                (Default: dump all pulsars)
 
         Outputs:
             None
     """
     # Grab the pulsar alias cache once rather than accessing it multiple times
     pulsaralias_cache = utils.get_pulsaralias_cache()
-    for id in sorted(pulsar_ids):
-        psrname = utils.get_pulsarname(id)
+    if pulsar_ids is None:
+        pulsar_ids = sorted(pulsaralias_cache.keys())
+    for psrid in sorted(pulsar_ids):
+        psrname = utils.get_pulsarname(psrid)
         print psrname
-        for alias in pulsaralias_cache[id]:
+        for alias in pulsaralias_cache[psrid]:
             if alias == psrname:
                 continue
             print alias
 
 
-
 def main(args):
-    # Get pulsar info
-    psrinfo = get_pulsarinfo()
-    print psrinfo
-    sys.exit(1)
+    # Build caches
+    utils.get_pulsarname_cache()
+    pulsar_ids = args.pulsar_ids + \
+                    [utils.get_pulsarid(psr) for psr in args.psrnames]
+    if not pulsar_ids:
+        pulsar_ids = None
     if args.output_style=='text':
-        show_pulsars(pulsar_ids)
+        # Get pulsar info
+        psrinfo = get_pulsarinfo(pulsar_ids)
+        show_pulsars(psrinfo)
     elif args.output_style=='dump':
         dump_pulsars(pulsar_ids)
     else:
