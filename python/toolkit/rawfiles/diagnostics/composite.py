1c9870c3d1815e2abeafd1e9c2231dd0c52a4e05
Patrick Lazarus <plazarus@mpifr.de>
2013-02-07 00:34:31 +0100

    Alter composite Diagnostic plot based on number of subints, channels, pols.

--- python/toolkit/rawfiles/diagnostics/composite.py
+++ python/toolkit/rawfiles/diagnostics/composite.py
@@ -13,12 +13,122 @@ class CompositePlotDiagnostic(base.PlotDiagnostic):
         handle, tmpfn = tempfile.mkstemp(suffix=".png")
         os.close(handle)
         params = utils.prep_file(self.fn)
+        
+        if (params['nsub'] > 1) and (params['nchan'] > 1):
+            self.__plot_all(tmpfn, params)
+        elif (params['nsub'] > 1) and (params['nchan'] == 1):
+            self.__plot_nofreq(tmpfn, params)
+        elif (params['nsub'] == 1) and (params['nchan'] > 1):
+            self.__plot_notime(tmpfn, params)
+        elif  (params['nsub'] == 1) and (params['nchan'] == 1):
+            self.__plot_profonly(tmpfn, params)
+        else:
+            raise errors.FileError("Not sure how to plot diagnostic for file. " \
+                                    "(nsub: %d; nchan: %d)" % \
+                                    (params['nsub'], params['nchan']))
+        tmpdir = os.path.split(tmpfn)[0]
+        archivefn = os.path.split(self.fn)[-1]
+        pngfn = os.path.join(tmpdir, archivefn+".composite.png")
+        shutil.move(tmpfn, pngfn) 
+        return pngfn
+
+    def __get_info(self, params):
+        info = "above:l=%s\n" \
+                       "%s    %s (%s)\n" \
+                       "Length=%.1f s    BW=%.1f MHz\n" \
+                       "N\\dbin\\u=$nbin    N\\dchan\\u=$nchan    N\\dsub\\u=$nsubint\n" \
+                       "Rawfile ID: %s," \
+               "above:off=3.5" % \
+                        (os.path.split(self.fn)[-1], \
+                         params['telescop'], params['rcvr'], \
+                         params['backend'], params['length'], params['bw'], \
+                         self.rawfile_id)
+        return info
+
+    def __plot_profonly(self, tmpfn, params):
+        if params['npol'] > 1:
+            profplot = 'stokes'
+            pol = ''
+        else:
+            profplot = 'flux'
+            pol = 'pol=I,'
+        info = self.__get_info(params)
         utils.execute("psrplot -O -j 'D' -c 'above:c=,x:range=0:2' %s -D %s/PNG " \
-                        "-p flux -c ':0:x:view=0.575:0.95," \
+                        "-p %s -c ':0:x:view=0.075:0.95," \
+                                       "y:view=0.15:0.7," \
+                                       "subint=I," \
+                                       "chan=I," \
+                                       "%s" \
+                                       "below:l=," \
+                                       "%s'" % \
+                            (self.fn, tmpfn, profplot, pol, info))
+        
+    def __plot_nofreq(self, tmpfn, params):
+        profplot = 'flux'
+        if params['npol'] > 1:
+            profplot = 'stokes'
+            pol = ''
+        else:
+            profplot = 'flux'
+            pol = 'pol=I,'
+        info = self.__get_info(params)
+        utils.execute("psrplot -O -j 'D' -c 'above:c=,x:range=0:2' %s -D %s/PNG " \
+                        "-p %s -c ':0:x:view=0.075:0.95," \
+                                       "y:view=0.5:0.7," \
+                                       "subint=I," \
+                                       "chan=I," \
+                                       "%s" \
+                                       "x:opt=BCTS," \
+                                       "x:lab=," \
+                                       "below:l=," \
+                                       "%s' " \
+                        "-p time -c ':1:x:view=0.075:0.95," \
+                                       "y:view=0.15:0.5," \
+                                       "chan=I," \
+                                       "pol=I," \
+                                       "cmap:map=plasma'" % \
+                            (self.fn, tmpfn, profplot, pol, info))
+        
+    def __plot_notime(self, tmpfn, params):
+        profplot = 'flux'
+        if params['npol'] > 1:
+            profplot = 'stokes'
+            pol = ''
+        else:
+            profplot = 'flux'
+            pol = 'pol=I,'
+        info = self.__get_info(params)
+        utils.execute("psrplot -O -j 'D' -c 'above:c=,x:range=0:2' %s -D %s/PNG " \
+                        "-p %s -c ':0:x:view=0.075:0.95," \
+                                       "y:view=0.5:0.7," \
+                                       "subint=I," \
+                                       "chan=I," \
+                                       "%s" \
+                                       "x:opt=BCTS," \
+                                       "x:lab=," \
+                                       "below:l=," \
+                                       "%s' " \
+                        "-p freq -c ':1:x:view=0.075:0.95," \
+                                       "y:view=0.15:0.5," \
+                                       "subint=I," \
+                                       "pol=I," \
+                                       "cmap:map=plasma'" % \
+                            (self.fn, tmpfn, profplot, pol, info))
+        
+    def __plot_all(self, tmpfn, params):
+        if params['npol'] > 1:
+            profplot = 'stokes'
+            pol = ''
+        else:
+            profplot = 'flux'
+            pol = 'pol=I,'
+        info = self.__get_info(params)
+        utils.execute("psrplot -O -j 'D' -c 'above:c=,x:range=0:2' %s -D %s/PNG " \
+                        "-p %s -c ':0:x:view=0.575:0.95," \
                                        "y:view=0.7:0.9," \
                                        "subint=I," \
                                        "chan=I," \
-                                       "pol=I," \
+                                       "%s" \
                                        "x:opt=BCTS," \
                                        "x:lab=," \
                                        "below:l=' " \
@@ -26,25 +136,14 @@ class CompositePlotDiagnostic(base.PlotDiagnostic):
                                        "y:view=0.15:0.7," \
                                        "subint=I," \
                                        "pol=I," \
-                                       "above:l=%s\n" \
-                                               "%s - %s (%s)\n" \
-                                               "Length=%.1f s - BW=%.1f MHz\n" \
-                                               "$nbin bins - $nchan chans - $nsubint subints," \
-                                       "above:off=3.5," \
+                                       "%s," \
                                        "cmap:map=plasma' " \
                         "-p time -c ':2:x:view=0.575:0.95," \
                                        "y:view=0.15:0.7," \
                                        "chan=I," \
                                        "pol=I," \
                                        "cmap:map=plasma'" % \
-                            (self.fn, tmpfn, os.path.split(self.fn)[-1], \
-                                params['telescop'], params['rcvr'], \
-                                params['backend'], params['length'], params['bw']))
-        tmpdir = os.path.split(tmpfn)[0]
-        archivefn = os.path.split(self.fn)[-1]
-        pngfn = os.path.join(tmpdir, archivefn+".composite.png")
-        shutil.move(tmpfn, pngfn) 
-        return pngfn
+                            (self.fn, tmpfn, profplot, pol, info))
 
 
 Diagnostic = CompositePlotDiagnostic
