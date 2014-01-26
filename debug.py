7e1b037815fe4773025dffe55db59cf7e5e48434
Patrick Lazarus <plazarus@mpifr.de>
2014-01-26 15:43:23 +0100

    Rewrite 'debug.py'.
    This introduced bugs elsewhere in the project.

--- debug.py
+++ debug.py
@@ -7,66 +7,65 @@ modules of a single process.
 Patrick Lazarus, Feb. 9, 2012
 """
 
-import errors
+from toaster import errors
 
-modes = [('syscalls', 'Print commands being executed as system calls.'), \
-         ('queries', 'Print database queries being executed.'), \
-         ('manipulator', "Print debugging info for manipulators."), \
-         ('gittest', "Raise warnings instead of errors when checking " \
-                        "git repos. This is useful for testing " \
-                        "un-committed changes."), \
-         ('config', "Display what config files are loaded."), \
-         ('database', "Display DB connection/transaction info."), \
-         ('toaparse', "Display info when parsing TOAs from timfiles."), \
-         ('timfile', None), 
-            ]
+MODE_DEFS = {'syscalls': 'Print commands being executed as system calls.',
+             'queries': 'Print database queries being executed.',
+             'manipulator': "Print debugging info for manipulators.",
+             'gittest': "Raise warnings instead of errors when checking "
+                        "git repos. This is useful for testing "
+                        "un-committed changes.",
+             'config': "Display what config files are loaded.",
+             'database': "Display DB connection/transaction info.",
+             'toaparse': "Display info when parsing TOAs from timfiles.",
+             'timfile': None}
 
-modes.sort()
-
-mode_names = []
-# By default set all debug modes to False
-for ii, (m, desc) in enumerate(modes):
-    mode_names.append(m)
-    exec("%s = False" % m.upper())
+ONMODES = {}
 
 
 def set_mode_on(*modes_toset):
-    for m in modes_toset:
-        if m not in mode_names:
-            raise errors.BadDebugMode("The debug mode '%s' doesn't exist!" % m)
-        exec "%s = True" % m.upper() in globals()
-
+    for mode in modes_toset:
+        if mode not in MODE_DEFS:
+            raise errors.BadDebugMode("The debug mode '%s' doesn't exist!" % mode)
+        ONMODES[mode] = True
 
 def set_allmodes_on():
-    for m, desc in modes:
-        exec "%s = True" % m.upper() in globals()
+    for mode in MODE_DEFS:
+        ONMODES[mode] = True
 
 
 def set_allmodes_off():
-    for m, desc in modes: 
-        exec "%s = False" % m.upper() in globals()
+    for mode in MODE_DEFS:
+        ONMODES[mode] = False
 
 
 def set_mode_off(*modes):
-    for m in modes:
-        exec "%s = False" % m.upper() in globals()
+    for mode in modes:
+        ONMODES[mode] = False
 
 
-def get_on_modes():     
-    on_modes = []                   
-    for m, desc in modes:           
-        if eval('%s' % m.upper()):
-            on_modes.append('debug.%s' % m.upper())
+def get_on_modes():
+    on_modes = []
+    for mode, is_it_on in ONMODES.iteritems():
+        if is_it_on:
+            on_modes.append(mode)
     return on_modes         
 
 
 def is_on(mode):
-    return eval('%s' % mode.upper())
+    return ONMODES[mode]
 
     
 def print_debug_status():
     on_modes = get_on_modes()
     print "The following debugging modes are turned on:"
-    for m in on_modes:      
-        print "    %s" % m  
-                          
+    if on_modes:
+        for mode in on_modes:
+            print "    %s" % mode
+    else:
+        print "    None"
+
+
+# By default set all debug modes to False
+set_allmodes_off()
+
