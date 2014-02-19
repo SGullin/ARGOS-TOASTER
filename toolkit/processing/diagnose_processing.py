4ae662799941ffe24277ef40b8bc1bb54cff938c
Patrick Lazarus <plazarus@mpifr.de>
2014-02-19 17:11:45 +0100

    Fix typo, imports and formatting in diagnose_processing.py'.

--- toolkit/processing/diagnose_processing.py
+++ toolkit/processing/diagnose_processing.py
@@ -6,10 +6,11 @@ Patrick Lazarus, Feb 8, 2013
 import os.path
 import shutil
 
-import utils
-import errors
-import database
-import diagnostics
+from toaster.utils import notify
+from toaster.utils import datafile
+from toaster import errors
+from toaster import database
+from toaster import diagnostics
 
 
 def check_processing_diagnostic_existence(proc_id, diagname, existdb=None):
@@ -31,20 +32,20 @@ def check_processing_diagnostic_existence(proc_id, diagname, existdb=None):
     db.connect()
 
     # Get float-valued diagnostics
-    select = db.select([db.proc_diagnostics.c.proc_diagnostic_id, \
+    select = db.select([db.proc_diagnostics.c.proc_diagnostic_id,
                         db.proc_diagnostics.c.value]).\
-                where((db.proc_diagnostics.c.type==diagname) & \
-                        (db.proc_diagnostics.c.process_id==proc_id))
+                where((db.proc_diagnostics.c.type == diagname) &
+                        (db.proc_diagnostics.c.process_id == proc_id))
     result = db.execute(select)
     floatrows = result.fetchall()
     result.close()
 
     # Get plot-valued diagnostics
-    select = db.select([db.proc_diagnostic_plots.c.proc_diagnostic_plot_id, \
-                        db.proc_diagnostic_plots.c.filename, \
+    select = db.select([db.proc_diagnostic_plots.c.proc_diagnostic_plot_id,
+                        db.proc_diagnostic_plots.c.filename,
                         db.proc_diagnostic_plots.c.filepath]).\
-                where((db.proc_diagnostic_plots.c.plot_type==diagname) & \
-                        (db.proc_diagnostic_plots.c.process_id==proc_id))
+                where((db.proc_diagnostic_plots.c.plot_type == diagname) &
+                        (db.proc_diagnostic_plots.c.process_id == proc_id))
     result = db.execute(select)
     plotrows = result.fetchall()
     result.close()
@@ -52,34 +53,44 @@ def check_processing_diagnostic_existence(proc_id, diagname, existdb=None):
     if existdb is None:
         db.close()
     if len(floatrows) > 1:
-        raise errors.InconsistentDatabaseError("There should be no " \
-                        "more than one float diagnostic of each type " \
-                        "per processing job. There are %d '%s' diagnostic " \
-                        "values for this processing job (ID: %d)" % \
-                        (len(floatrows), diagname, proc_id))
+        raise errors.InconsistentDatabaseError("There should be no "
+                                               "more than one float "
+                                               "diagnostic of each type "
+                                               "per processing job. There "
+                                               "are %d '%s' diagnostic "
+                                               "values for this processing "
+                                               "job (ID: %d)" %
+                                               (len(floatrows), diagname,
+                                                proc_id))
     elif len(floatrows) == 1:
         val = floatrows[0]['value']
-        raise errors.DiagnosticAlreadyExists("There is already a '%s' float " \
-                        "diagnostic value in the DB for this processing job " \
-                        "(ID: %d; value: %g)" % \
-                        (diagname, rawfile_id, val))
+        raise errors.DiagnosticAlreadyExists("There is already a '%s' float "
+                                             "diagnostic value in the DB for "
+                                             "this processing job "
+                                             "(ID: %d; value: %g)" %
+                                             (diagname, proc_id, val))
 
     if len(plotrows) > 1:
-        raise errors.InconsistentDatabaseError("There should be no " \
-                        "more than one diagnostic plot of each type " \
-                        "per processing job. There are %d '%s' diagnostic " \
-                        "plots for this processing job (ID: %d)" % \
-                        (len(plotrows), diagname, proc_id))
+        raise errors.InconsistentDatabaseError("There should be no "
+                                               "more than one diagnostic "
+                                               "plot of each type "
+                                               "per processing job. There "
+                                               "are %d '%s' diagnostic "
+                                               "plots for this processing "
+                                               "job (ID: %d)" %
+                                               (len(plotrows), diagname,
+                                                proc_id))
     elif len(plotrows) == 1:
         fn = os.path.join(plotrows[0]['filepath'], plotrows[0]['filename'])
-        raise errors.DiagnosticAlreadyExists("There is already a '%s' " \
-                        "diagnostic plot in the DB for this processing job " \
-                        "(ID: %d; file: %s)" % \
-                        (diagname, proc_id, fn))
+        raise errors.DiagnosticAlreadyExists("There is already a '%s' "
+                                             "diagnostic plot in the DB for "
+                                             "this processing job "
+                                             "(ID: %d; file: %s)" %
+                                             (diagname, proc_id, fn))
 
 
-def insert_processing_diagnostics(proc_id, diags, archivedir=None, \
-                                    suffix="", existdb=None):
+def insert_processing_diagnostics(proc_id, diags, archivedir=None,
+                                  suffix="", existdb=None):
     """Insert processing diagnostics, carefully checking if each
         diagnostic is float-valued, or plot-valued.
  
@@ -109,15 +120,15 @@ def insert_processing_diagnostics(proc_id, diags, archivedir=None, \
                 if isinstance(diag, diagnostics.base.FloatDiagnostic):
                     __insert_processing_float_diagnostic(proc_id, diag, existdb=db)
                 elif isinstance(diag, diagnostics.base.PlotDiagnostic):
-                    __insert_processing_diagnostic_plot(proc_id, diag, \
-                                                    archivedir=archivedir, \
-                                                    suffix=suffix, existdb=db)
+                    __insert_processing_diagnostic_plot(proc_id, diag,
+                                                        archivedir=archivedir,
+                                                        suffix=suffix, existdb=db)
                 else:
-                    raise ValueError("Diagnostic is not a valid type (%s)!" % \
-                                        type(diag))
+                    raise ValueError("Diagnostic is not a valid type (%s)!" %
+                                     type(diag))
             except errors.DiagnosticAlreadyExists, e:
-                print_info("Diagnostic already exists: %s. Skipping..." % \
-                                str(e), 2)
+                notify.print_info("Diagnostic already exists: %s. "
+                                  "Skipping..." % str(e), 2)
                 db.rollback()
             else:
                 db.commit()
@@ -146,14 +157,14 @@ def __insert_processing_float_diagnostic(proc_id, diag, existdb=None):
 
     try:
         ins = db.proc_diagnostics.insert()
-        values = {'process_id':proc_id, \
-                  'value':diag.diagnostic, \
-                  'type':diag.name}
+        values = {'process_id': proc_id,
+                  'value': diag.diagnostic,
+                  'type': diag.name}
         result = db.execute(ins, values)
         diag_id = result.inserted_primary_key[0]
         result.close()
-        utils.print_info("Inserted process diagnostic (type: %s)." % \
-                    diag.name, 2)
+        notify.print_info("Inserted process diagnostic (type: %s)." %
+                          diag.name, 2)
     finally:
         if not existdb:
             # Close DB connection
@@ -161,8 +172,8 @@ def __insert_processing_float_diagnostic(proc_id, diag, existdb=None):
     return diag_id
 
     
-def __insert_processing_diagnostic_plot(proc_id, diag, archivedir=None, \
-                                    suffix="", existdb=None):
+def __insert_processing_diagnostic_plot(proc_id, diag, archivedir=None,
+                                        suffix="", existdb=None):
     """Insert processing plot diagnostic.
 
         Inputs:
@@ -195,27 +206,27 @@ def __insert_processing_diagnostic_plot(proc_id, diag, archivedir=None, \
             stem, ext = os.path.splitext(origfn)
             newfn = stem+suffix+ext
             if os.path.dirname(newfn) != os.path.dirname(origfn):
-                raise errors.FileError("Adding processing diagnostic " \
-                            "plot suffix will cause plot to be moved " \
-                            "to annother directory (new file name: %s)!" % \
-                            newfn)
+                raise errors.FileError("Adding processing diagnostic "
+                                       "plot suffix will cause plot to "
+                                       "be moved to annother directory "
+                                       "(new file name: %s)!" % newfn)
             shutil.move(origfn, newfn)
             diagplot = newfn
         else:
             diagplot = origfn
-        diagpath = utils.archive_file(diagplot, archivedir)
+        diagpath = datafile.archive_file(diagplot, archivedir)
         diagdir, diagfn = os.path.split(os.path.abspath(diagpath))
     
         ins = db.proc_diagnostic_plots.insert()
-        values = {'process_id':proc_id, \
-                  'filename':diagfn, \
-                  'filepath':diagdir, \
-                  'plot_type':diag.name}
+        values = {'process_id': proc_id,
+                  'filename': diagfn,
+                  'filepath': diagdir,
+                  'plot_type': diag.name}
         result = db.execute(ins, values)
         diag_id = result.inserted_primary_key[0]
         result.close()
-        utils.print_info("Inserted process diagnostic plot (type: %s)." % \
-                    diag.name, 2)
+        notify.print_info("Inserted process diagnostic plot (type: %s)." %
+                          diag.name, 2)
     except:
         # Move the diagnostic plot back if it has already been archived.
         if diagpath and os.path.isfile(diagpath):
