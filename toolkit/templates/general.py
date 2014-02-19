00bd611ec01bcbbe1d1dac8e6635b0a9a50ff032
Patrick Lazarus <plazarus@mpifr.de>
2014-02-19 15:02:58 +0100

    Fix 'templates/general.py' and 'load_template.py' to adhere to new utils-modules' organization.

--- toolkit/templates/general.py
+++ toolkit/templates/general.py
@@ -1,8 +1,10 @@
+import os.path
+import warnings
+
 from toaster import database
 from toaster import errors
-from toaster import utils
+from toaster.utils import datafile
 from toaster.utils import notify
-from utils.datafile import get_md5sum
 
 
 def set_as_master_template(template_id, existdb=None):
@@ -19,32 +21,33 @@ def set_as_master_template(template_id, existdb=None):
         Ouputs:
             None
     """
-    # Use the exisitng DB connection, or open a new one if None was provided
+    # Use the existng DB connection, or open a new one if None was provided
     db = existdb or database.Database()
     db.connect()
     
     trans = db.begin()
-    # Check if this pulsar/obssystem combiation already has a
+    # Check if this pulsar/obssystem combination already has a
     # Master template in the DB
-    select = db.select([db.master_templates.c.template_id.label('mtempid'), \
-                        db.templates.c.pulsar_id, \
+    select = db.select([db.master_templates.c.template_id.label('mtempid'),
+                        db.templates.c.pulsar_id,
                         db.templates.c.obssystem_id]).\
-                where((db.master_templates.c.obssystem_id == \
-                                db.templates.c.obssystem_id) & \
-                        (db.master_templates.c.pulsar_id == \
-                                db.templates.c.pulsar_id) & \
-                        (db.templates.c.template_id==template_id))
+                where((db.master_templates.c.obssystem_id ==
+                                db.templates.c.obssystem_id) &
+                        (db.master_templates.c.pulsar_id ==
+                                db.templates.c.pulsar_id) &
+                        (db.templates.c.template_id == template_id))
     result = db.execute(select)
     row = result.fetchone()
     result.close()
     if row:
         if row['mtempid'] == template_id:
-            warnings.warn("Template (ID: %d) is already the master " \
-                            "template for this pulsar (ID: %d), " \
-                            "observing system (ID: %d) combination. " \
-                            "Doing nothing..." % (row['mtempid'], \
-                            row['pulsar_id'], row['obssystem_id']), \
-                            errors.ToasterWarning)
+            warnings.warn("Template (ID: %d) is already the master "
+                          "template for this pulsar (ID: %d), "
+                          "observing system (ID: %d) combination. "
+                          "Doing nothing..." %
+                          (row['mtempid'],row['pulsar_id'],
+                           row['obssystem_id']),
+                          errors.ToasterWarning)
             trans.commit()
             if not existdb:
                 db.close()
@@ -52,24 +55,24 @@ def set_as_master_template(template_id, existdb=None):
         else:
             # Update the existing entry
             query = db.master_templates.update().\
-                        where((db.master_templates.c.pulsar_id == \
-                                    row['pulsar_id']) & \
-                              (db.master_templates.c.obssystem_id == \
+                        where((db.master_templates.c.pulsar_id ==
+                                    row['pulsar_id']) &
+                              (db.master_templates.c.obssystem_id ==
                                     row['obssystem_id']))
-            values = {'template_id':template_id}
+            values = {'template_id': template_id}
     else:
         # Insert a new entry
         query = db.master_templates.insert()
-        select = db.select([db.templates.c.pulsar_id, \
+        select = db.select([db.templates.c.pulsar_id,
                             db.templates.c.obssystem_id]).\
-                    where(db.templates.c.template_id==template_id)
+                    where(db.templates.c.template_id == template_id)
         result = db.execute(select)
         row = result.fetchone()
         result.close()
 
-        values = {'template_id':template_id, \
-                  'pulsar_id':row['pulsar_id'], \
-                  'obssystem_id':row['obssystem_id']}
+        values = {'template_id': template_id,
+                  'pulsar_id': row['pulsar_id'],
+                  'obssystem_id': row['obssystem_id']}
     try:
         result = db.execute(query, values)
     except:
@@ -99,14 +102,14 @@ def get_template_id(template, existdb=None):
     db.connect()
     
     notify.print_info("Getting template ID for %s using "
-                    "filename and md5sum" % args.parfile, 2)
+                      "filename and md5sum" % template, 2)
     path, fn = os.path.split(os.path.abspath(template))
-    md5sum = get_md5sum(template)
-    select = db.select([db.templates.c.template_id, \
-                        db.templates.c.filename, \
+    md5sum = datafile.get_md5sum(template)
+    select = db.select([db.templates.c.template_id,
+                        db.templates.c.filename,
                         db.templates.c.md5sum]).\
-                where((db.template.c.md5sum==md5sum) | (
-                        db.templates.c.filename==fn))
+                where((db.template.c.md5sum == md5sum) |
+                      (db.templates.c.filename == fn))
     result = db.execute(select)
     rows = result.fetchall()
     result.close()
@@ -114,31 +117,36 @@ def get_template_id(template, existdb=None):
         db.close()
     if len(rows) == 1:
         row = rows[0]
-        if row['md5sum']==md5sum and row['filename']==fn:
+        if row['md5sum'] == md5sum and row['filename'] == fn:
             return row['template_id']
-        elif row['md5sum']==md5sum:
-            raise errors.FileError("A template (template_id=%d) with " \
-                            "this md5sum, but a different filename " \
-                            "exists in the DB." % row['template_id'])
-        elif row['filename']==fn:
-            raise errors.FileError("A template (template_id=%d) with " \
-                            "this filename, but a different md5sum " \
-                            "exists in the DB." % row['template_id'])
+        elif row['md5sum'] == md5sum:
+            raise errors.FileError("A template (template_id=%d) with "
+                                   "this md5sum, but a different filename "
+                                   "exists in the DB." % row['template_id'])
+        elif row['filename'] == fn:
+            raise errors.FileError("A template (template_id=%d) with "
+                                   "this filename, but a different md5sum "
+                                   "exists in the DB." % row['template_id'])
         else:
-            raise errors.InconsistentDatabaseError("A template (template_id=%d) " \
-                            "matches our query, but neither its md5sum (%s), " \
-                            "nor its filename (%s) appears to match! " \
-                            "This should never happen!" % 
-                            (row['template_id'], row['md5sum'], row['fn']))
+            raise errors.InconsistentDatabaseError("A template "
+                                                   "(template_id=%d) "
+                                                   "matches our query, but "
+                                                   "neither its md5sum (%s), "
+                                                   "nor its filename (%s) "
+                                                   "appears to match! "
+                                                   "This should never "
+                                                   "happen!" %
+                                                   (row['template_id'],
+                                                    row['md5sum'], row['fn']))
     elif len(rows) == 0:
-        raise errors.ToasterError("Input template (%s) does not appear " \
-                                        "to be registered in the DB! " \
-                                        "Use 'load_template.py' to load " \
-                                        "it into the DB." % template)
+        raise errors.ToasterError("Input template (%s) does not appear "
+                                  "to be registered in the DB! "
+                                  "Use 'load_template.py' to load "
+                                  "it into the DB." % template)
     else:
-        raise errors.InconsistentDatabaseError("Multiple (%s) templates " \
-                                    "match the given file name or md5sum!" % \
-                                    len(rows))
+        raise errors.InconsistentDatabaseError("Multiple (%s) templates "
+                                               "match the given file name "
+                                               "or md5sum!" % len(rows))
 
 
 def get_template_from_id(template_id, existdb=None, verify_md5=True):
@@ -163,10 +171,10 @@ def get_template_from_id(template_id, existdb=None, verify_md5=True):
     db = existdb or database.Database()
     db.connect()
     
-    select = db.select([db.templates.c.filename, \
-                        db.templates.c.filepath, \
+    select = db.select([db.templates.c.filename,
+                        db.templates.c.filepath,
                         db.templates.c.md5sum]).\
-                where(db.templates.c.template_id==template_id)
+                where(db.templates.c.template_id == template_id)
     result = db.execute(select)
     rows = result.fetchall()
     result.close()
@@ -177,23 +185,24 @@ def get_template_from_id(template_id, existdb=None, verify_md5=True):
     if len(rows) == 1:
         filename = rows[0]['filename']
         filepath = rows[0]['filepath']
-        md5sum_DB = rows[0]['md5sum']
+        md5sum_db = rows[0]['md5sum']
     else:
-        raise errors.InconsistentDatabaseError("Bad number of files (%d) " \
-                            "with template_id=%d" % (len(rows), template_id))
+        raise errors.InconsistentDatabaseError("Bad number of files (%d) "
+                                               "with template_id=%d" %
+                                               (len(rows), template_id))
         
-    fullpath = os.path.join(filepath,filename)
+    fullpath = os.path.join(filepath, filename)
     # Make sure the file exists
-    utils.Verify_file_path(fullpath)
+    datafile.verify_file_path(fullpath)
     if verify_md5:
-        notify.print_info("Confirming MD5 sum of %s matches what is " \
-                    "stored in DB (%s)" % (fullpath, md5sum_DB), 2)
+        notify.print_info("Confirming MD5 sum of %s matches what is "
+                          "stored in DB (%s)" % (fullpath, md5sum_db), 2)
                     
-        md5sum_file = get_md5sum(fullpath)
-        if md5sum_DB != md5sum_file:
-            raise errors.FileError("md5sum check of %s failed! MD5 from " \
-                                "DB (%s) != MD5 from file (%s)" % \
-                                (fullpath, md5sum_DB, md5sum_file))
+        md5sum_file = datafile.get_md5sum(fullpath)
+        if md5sum_db != md5sum_file:
+            raise errors.FileError("md5sum check of %s failed! MD5 from "
+                                   "DB (%s) != MD5 from file (%s)" %
+                                   (fullpath, md5sum_db, md5sum_file))
     return fullpath
 
 
@@ -218,21 +227,22 @@ def get_master_template(pulsar_id, obssystem_id):
     db = database.Database()
     db.connect()
 
-    select = db.select([db.templates.c.template_id, \
-                        db.templates.c.filename, \
+    select = db.select([db.templates.c.template_id,
+                        db.templates.c.filename,
                         db.templates.c.filepath]).\
-                where((db.master_templates.c.template_id == \
-                            db.templates.c.template_id) & \
-                      (db.master_templates.c.pulsar_id == pulsar_id) & \
+                where((db.master_templates.c.template_id ==
+                            db.templates.c.template_id) &
+                      (db.master_templates.c.pulsar_id == pulsar_id) &
                       (db.master_templates.c.obssystem_id == obssystem_id))
     result = db.execute(select)
     rows = result.fetchall()
     result.close()
     db.close()
     if len(rows) > 1:
-        raise errors.InconsistentDatabaseError("There are too many (%d) " \
-                                            "master templates for pulsar #%d" % \
-                                            (len(rows), pulsar_id ))
+        raise errors.InconsistentDatabaseError("There are too many (%d) "
+                                               "master templates for pulsar "
+                                               "#%d" %
+                                               (len(rows), pulsar_id))
     elif len(rows) == 0:
         return None, None
     else:
--- toolkit/templates/load_template.py
+++ toolkit/templates/load_template.py
@@ -12,6 +12,9 @@ import shlex
 import database
 import errors
 import utils
+from toaster.utils import notify
+from toaster.utils import datafile
+from toolkit.templates import general
 
 
 SHORTNAME = 'load'
@@ -20,60 +23,65 @@ DESCRIPTION = "Upload a standard template " \
 
 
 def add_arguments(parser):
-    parser.add_argument('--master', dest='is_master', \
-                         action = 'store_true', default=False, \
-                         help = "Whether or not the provided file is to be " \
-                                "set as the master template.")
-    parser.add_argument('--comments', dest='comments', type=str, \
+    parser.add_argument('--master', dest='is_master',
+                        action='store_true', default=False,
+                        help="Whether or not the provided file is to be "
+                             "set as the master template.")
+    parser.add_argument('--comments', dest='comments', type=str,
                         help="Provide comments describing the template.")
-    parser.add_argument('--from-file', dest='from_file', \
-                        type=str, default=None, \
-                        help="A list of templates (one per line) to " \
-                            "load. Note: each line can also include " \
-                            "flags to override what was provided on " \
-                            "the cmd line for that template. (Default: " \
-                            "load a single template provided on the " \
-                            "cmd line.)")
-    parser.add_argument('template', nargs='?', type=str, \
+    parser.add_argument('--from-file', dest='from_file',
+                        type=str, default=None,
+                        help="A list of templates (one per line) to "
+                             "load. Note: each line can also include "
+                             "flags to override what was provided on "
+                             "the cmd line for that template. (Default: "
+                             "load a single template provided on the "
+                             "cmd line.)")
+    parser.add_argument('template', nargs='?', type=str,
                         help="File name of the template to upload.")
 
 
 def populate_templates_table(db, fn, params, comments):
     if comments is None:
-        raise errors.BadInputError("A comment is required for every " \
-                                        "template!")
+        raise errors.BadInputError("A comment is required for every "
+                                   "template!")
     # md5sum helper function in utils 
-    md5 = utils.Get_md5sum(fn);
+    md5 = datafile.get_md5sum(fn)
     path, fn = os.path.split(os.path.abspath(fn))
     
     trans = db.begin()
     # Does this file exist already?
-    select = db.select([db.templates.c.template_id, \
+    select = db.select([db.templates.c.template_id,
                         db.templates.c.pulsar_id]).\
-                where(db.templates.c.md5sum==md5)
+                where(db.templates.c.md5sum == md5)
     results = db.execute(select)
     rows = results.fetchall()
     results.close()
     if len(rows) > 1:
         db.rollback()
-        raise errors.InconsistentDatabaseError("There are %d templates " \
-                    "with MD5 (%s) in the database already" % (len(rows), md5))
+        raise errors.InconsistentDatabaseError("There are %d templates "
+                                               "with MD5 (%s) in the "
+                                               "database already" %
+                                               (len(rows), md5))
     elif len(rows) == 1:
         psr_id = rows[0]['pulsar_id']
         template_id = rows[0]['template_id']
         if psr_id == params['pulsar_id']:
             db.commit()
-            warnings.warn("A template with this MD5 (%s) already exists " \
-                            "in the DB for this pulsar (ID: %d). " \
-                            "The file will not be re-registed into the DB. " \
-                            "Doing nothing..." % (md5, psr_id), \
-                            errors.ToasterWarning)
+            warnings.warn("A template with this MD5 (%s) already exists "
+                          "in the DB for this pulsar (ID: %d). "
+                          "The file will not be re-registed into the DB. "
+                          "Doing nothing..." % (md5, psr_id),
+                          errors.ToasterWarning)
             return template_id
         else:
             db.rollback()
-            raise errors.InconsistentDatabaseError("A template with this " \
-                            "MD5 (%s) already exists in the DB, but for " \
-                            "a different pulsar (ID: %d)!" % (md5, psr_id))
+            raise errors.InconsistentDatabaseError("A template with this "
+                                                   "MD5 (%s) already exists "
+                                                   "in the DB, but for "
+                                                   "a different pulsar "
+                                                   "(ID: %d)!" %
+                                                   (md5, psr_id))
     else:
         # Based on its MD5, this template doesn't already 
         # exist in the DB.
@@ -81,27 +89,28 @@ def populate_templates_table(db, fn, params, comments):
         # Check to see if this pulsar/observing system combination
         # Already has a template
         select = db.select([db.templates.c.template_id]).\
-                    where((db.templates.c.pulsar_id==params['pulsar_id']) & \
-                          (db.templates.c.obssystem_id==params['obssystem_id']))
+                    where((db.templates.c.pulsar_id == params['pulsar_id']) &
+                          (db.templates.c.obssystem_id == params['obssystem_id']))
         results = db.execute(select)
         rows = results.fetchall()
         results.close()
         if len(rows):
-            warnings.warn("This pulsar_id (%d), obssystem_id (%d) " \
-                        "combination already has %d templates in the DB. " \
-                        "Be sure to correctly set the master template." % \
-                        (params['pulsar_id'], params['obssystem_id'], len(rows)))
+            warnings.warn("This pulsar_id (%d), obssystem_id (%d) "
+                          "combination already has %d templates in the DB. "
+                          "Be sure to correctly set the master template." %
+                          (params['pulsar_id'], params['obssystem_id'],
+                           len(rows)))
 
         # Insert the template
         ins = db.templates.insert()
-        values = {'md5sum':md5, \
-                  'filename':fn, \
-                  'filepath': path, \
-                  'user_id':params['user_id'], \
-                  'pulsar_id':params['pulsar_id'], \
-                  'obssystem_id':params['obssystem_id'], \
-                  'nbin':params['nbin'], \
-                  'comments':comments}
+        values = {'md5sum': md5,
+                  'filename': fn,
+                  'filepath': path,
+                  'user_id': params['user_id'],
+                  'pulsar_id': params['pulsar_id'],
+                  'obssystem_id': params['obssystem_id'],
+                  'nbin': params['nbin'],
+                  'comments': comments}
         result = db.execute(ins, values)
         template_id = result.inserted_primary_key[0]
         result.close()
@@ -116,32 +125,32 @@ def load_template(fn, comments, is_master=False, existdb=None):
 
     try:
         # Now load the template file into database
-        utils.print_info("Working on %s (%s)" % (fn, utils.Give_UTC_now()), 1)
+        notify.print_info("Working on %s (%s)" % (fn, utils.give_utc_now()), 1)
         
         # Check the template and parse the header
-        params = utils.prep_file(fn)
+        params = datafile.prep_file(fn)
         
         # Move the file
-        destdir = utils.get_archive_dir(fn, params=params)
-        newfn = utils.archive_file(fn, destdir)
+        destdir = datafile.get_archive_dir(fn, params=params)
+        newfn = datafile.archive_file(fn, destdir)
  
         # Register the template into the database
-        template_id = populate_templates_table(db, newfn, params, \
-                        comments=comments)
+        template_id = populate_templates_table(db, newfn, params,
+                                               comments=comments)
 
-        mastertemp_id, tempfn = utils.get_master_template(params['pulsar_id'], \
-                                                        params['obssystem_id'])
+        mastertemp_id, tempfn = utils.get_master_template(params['pulsar_id'],
+                                                          params['obssystem_id'])
         if mastertemp_id is None:
             # If this is the only template for this pulsar
             # make sure it will be set as the master
             is_master = True
 
         if is_master:
-            utils.print_info("Setting %s as master template (%s)" % \
-                            (newfn, utils.Give_UTC_now()), 1)
-            utils.set_as_master_template(template_id, db)
-        utils.print_info("Finished with %s - template_id=%d (%s)" % \
-                        (fn, template_id, utils.Give_UTC_now()), 1)
+            notify.print_info("Setting %s as master template (%s)" %
+                              (newfn, utils.Give_UTC_now()), 1)
+            general.set_as_master_template(template_id, db)
+        notify.print_info("Finished with %s - template_id=%d (%s)" %
+                          (fn, template_id, utils.Give_UTC_now()), 1)
     finally:
         if not existdb:
             # Close DB connection
@@ -151,11 +160,9 @@ def load_template(fn, comments, is_master=False, existdb=None):
 
 def main(args):
     # Allow reading input from stdin
-    if ((args.template is None) or (args.template == '-')) and \
-                (args.from_file is None):
-        warnings.warn("No input file or --from-file argument given " \
-                        "will read from stdin.", \
-                        errors.ToasterWarning)
+    if ((args.template is None) or (args.template == '-')) and (args.from_file is None):
+        warnings.warn("No input file or --from-file argument given "
+                      "will read from stdin.", errors.ToasterWarning)
         args.template = None # In case it was set to '-'
         args.from_file = '-'
     
@@ -169,17 +176,17 @@ def main(args):
             parser = utils.DefaultArguments()
             add_arguments(parser)
             if args.template is not None:
-                raise errors.BadInputError("When loading templates from " \
-                                "a file, a template value should _not_ be " \
-                                "provided on the command line. (The value " \
-                                "%s was given on the command line)." % \
-                                args.template)
+                raise errors.BadInputError("When loading templates from a file, "
+                                           "a template value should _not_ be "
+                                           "provided on the command line. (The "
+                                           "value %s was given on the command "
+                                           "line)." % args.template)
             if args.from_file == '-':
                 templatelist = sys.stdin
             else:
                 if not os.path.exists(args.from_file):
-                    raise errors.FileError("The template list (%s) does " \
-                                "not appear to exist." % args.from_file)
+                    raise errors.FileError("The template list (%s) does "
+                                           "not appear to exist." % args.from_file)
                 templatelist = open(args.from_file, 'r')
             numfails = 0
             numloaded = 0
@@ -195,8 +202,8 @@ def main(args):
                     parser.parse_args(arglist, namespace=customargs)
                  
                     fn = customargs.template
-                    template_id = load_template(fn, customargs.comments, \
-                                            customargs.is_master, db)
+                    template_id = load_template(fn, customargs.comments,
+                                                customargs.is_master, db)
                     print "%s has been loaded to the DB. template_id: %d" % \
                         (fn, template_id)
                     numloaded += 1
@@ -206,26 +213,26 @@ def main(args):
             if args.from_file != '-':
                 templatelist.close()
             if numloaded:
-                utils.print_success("\n\n===================================\n" \
-                                    "%d templates successfully loaded\n" \
+                utils.print_success("\n\n===================================\n"
+                                    "%d templates successfully loaded\n"
                                     "===================================\n" % numloaded)
             if numfails:
                 raise errors.ToasterError(\
-                    "\n\n===================================\n" \
-                        "The loading of %d templates failed!\n" \
-                        "Please review error output.\n" \
-                        "===================================\n" % numfails)
+                    "\n\n===================================\n"
+                    "The loading of %d templates failed!\n"
+                    "Please review error output.\n"
+                    "===================================\n" % numfails)
         else:
             fn = args.template
             template_id = load_template(fn, args.comments, args.is_master, db)
             print "%s has been loaded to the DB. template_id: %d" % \
-                    (fn, template_id)
+                  (fn, template_id)
     finally:
         # Close DB connection
         db.close()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = utils.DefaultArguments(description=DESCRIPTION)
     add_arguments(parser)
     args = parser.parse_args()
