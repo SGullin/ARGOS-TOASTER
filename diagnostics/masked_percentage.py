6505cc0ec55cd94f42527179dd5e0922d1f5e742
Patrick Lazarus <plazarus@mpifr.de>
2014-02-19 17:03:24 +0100

    Fix imports and formatting in 'masked_percentage.py'.

--- diagnostics/masked_percentage.py
+++ diagnostics/masked_percentage.py
@@ -1,19 +1,21 @@
 import numpy as np
 
-import utils
-import base
+from toaster import utils
+from toaster.utils import notify
+from toaster.diagnostics import base
+
 
 class MaskedPercentageDiagnostic(base.FloatDiagnostic):
     name = "Masked Percentage"
     description = "Percentage of profiles that are fully masked."
 
     def _compute(self):
-        utils.print_info("Calling psrstat to get weights for %s" % self.fn, 3)
+        notify.print_info("Calling psrstat to get weights for %s" % self.fn, 3)
         cmd = ["psrstat", self.fn, "-c", "int:wt", "-Qq"]
         outstr, errstr = utils.execute(cmd)
         wtstrs = outstr.strip().split(',')
         weights = np.array([float(wt) for wt in wtstrs])
-        maskpcnt = 100.0*np.sum(weights>0)/weights.size
+        maskpcnt = 100.0*np.sum(weights > 0)/weights.size
         return maskpcnt
 
 
