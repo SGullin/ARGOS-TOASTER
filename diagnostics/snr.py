8a009f685944d02668f6d3a903b74df881bfd850
Patrick Lazarus <plazarus@mpifr.de>
2014-02-19 16:57:54 +0100

    Fix imports and formatting in 'snr.py'.

--- diagnostics/snr.py
+++ diagnostics/snr.py
@@ -1,13 +1,15 @@
-import utils
-import base
+from toaster import utils
+from toaster.diagnostics import base
+from toaster.utils import notify
+
 
 class SNRDiagnostic(base.FloatDiagnostic):
     name = "SNR"
     description = "Signal-to-noise ratio for the fully scrunched " \
-                    "data as determined by 'psrstat'."
+                  "data as determined by 'psrstat'."
 
     def _compute(self):
-        utils.print_info("Calling psrstat to get SNR for %s" % self.fn, 3)
+        notify.print_info("Calling psrstat to get SNR for %s" % self.fn, 3)
         cmd = ["psrstat", "-Qq", "-j", "DTFp", "-c", "snr", self.fn]
         outstr, errstr = utils.execute(cmd)
         snr = float(outstr)
