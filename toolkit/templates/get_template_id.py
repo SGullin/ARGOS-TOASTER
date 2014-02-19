6f11fe4a336e00f522320a1bb77497fa41bbc63a
Patrick Lazarus <plazarus@mpifr.de>
2014-02-19 15:21:55 +0100

    Fix to 'get_template_id.py' to adhere to new utils-modules' organization.

--- toolkit/templates/get_template_id.py
+++ toolkit/templates/get_template_id.py
@@ -8,84 +8,80 @@ input is most appropriate.
 Patrick Lazarus, Apr. 8, 2012.
 """
 
-import argparse
 import os
-import os.path
-import datetime
-import warnings
 
-import config
-import utils
-import database
-import errors
-import colour
+from toaster import utils
+from toaster.utils import notify
+from toaster import database
+from toaster import errors
+from toaster import colour
 
 
 SHORTNAME = 'show'
-DESCRIPTION = "Query the database for template information." \
+DESCRIPTION = "Query the database for template information."
 
 
 def add_arguments(parser):
-    parser.add_argument('--template-id', dest='template_id', \
-                        type=int, default=None, \
-                        help="A template ID. This is useful for checking " \
-                            "the details of a single template, identified " \
-                            "by its ID number. NOTE: No other templates " \
-                            "will match if this option is provided.")
-    parser.add_argument('-p', '--psr', dest='pulsar_name', \
-                        type=str, default='%', \
-                        help="The pulsar to grab templates for. " \
-                            "NOTE: SQL regular expression syntax may be used")
-    parser.add_argument('-s', '--start-date', dest='start_date', \
-                        type=str, default=None, \
-                        help="Do not return templates added to the DB " \
-                            "before this date.")
-    parser.add_argument('-e', '--end-date', dest='end_date', \
-                        type=str, default=None, \
-                        help="Do not return templates added to the DB " \
-                            "after this date.")
-    parser.add_argument('-i', '--id', dest='ids', type=int, \
-                        default=[], action='append', \
+    parser.add_argument('--template-id', dest='template_id',
+                        type=int, default=None,
+                        help="A template ID. This is useful for checking "
+                             "the details of a single template, identified "
+                             "by its ID number. NOTE: No other templates "
+                             "will match if this option is provided.")
+    parser.add_argument('-p', '--psr', dest='pulsar_name',
+                        type=str, default='%',
+                        help="The pulsar to grab templates for. "
+                             "NOTE: SQL regular expression syntax may be used")
+    parser.add_argument('-s', '--start-date', dest='start_date',
+                        type=str, default=None,
+                        help="Do not return templates added to the DB "
+                             "before this date.")
+    parser.add_argument('-e', '--end-date', dest='end_date',
+                        type=str, default=None,
+                        help="Do not return templates added to the DB "
+                             "after this date.")
+    parser.add_argument('-i', '--id', dest='ids', type=int,
+                        default=[], action='append',
                         help="Specific template_id numbers to describe.")
-    parser.add_argument('-n', '--nbin', dest='nbin', \
-                        type=int, default=None, \
-                        help="Only show templates with a specific number " \
-                            "of bins. NOTE: this will exclude analytic " \
-                            "templates.")
-    parser.add_argument('--obssystem-id', dest='obssys_id', \
-                        type=int, default=None, \
-                        help="Grab templates from a specific observing system. " \
-                            "NOTE: the argument should be the obssystem_id " \
-                            "from the database. " \
-                            "(Default: No constraint on obssystem_id.)")
-    parser.add_argument('--obssystem-name', dest='obssystem_name', \
-                        type=int, default=None, \
-                        help="Grab templates from a specific observing system. " \
-                            "NOTE: the argument should be the name of the " \
-                            "observing system as recorded in the database. " \
-                            "NOTE: SQL regular expression syntax may be used " \
-                            "(Default: No constraint on obs system's name.)")
-    parser.add_argument('-t', '--telescope', dest='telescope', \
-                        type=str, default=None, \
-                        help="Grab templates from specific telescopes. " \
-                            "The telescope's _name_ must be used here. " \
-                            "NOTE: SQL regular expression syntax may be used " \
-                            "(Default: No constraint on telescope name.)")
-    parser.add_argument('-f', '--frontend', dest='frontend', \
-                        type=str, default=None, \
-                        help="Grab templates from specific frontends. " \
-                            "NOTE: SQL regular expression syntax may be used " \
-                            "(Default: No constraint on frontend name.)")
-    parser.add_argument('-b', '--backend', dest='backend', \
-                        type=str, default=None, \
-                        help="Grab templates from specific backends. " \
-                            "NOTE: SQL regular expression syntax may be used " \
-                            "(Default: No constraint on backend name.)")
-    parser.add_argument('-c', '--clock', dest='clock', \
-                        type=str, default=None, \
-                        help="Grab templates from specific clocks. " \
-                            "NOTE: SQL regular expression syntax may be used " \
-                            "(Default: No constraint on clock name.)") 
+    parser.add_argument('-n', '--nbin', dest='nbin',
+                        type=int, default=None,
+                        help="Only show templates with a specific number "
+                             "of bins. NOTE: this will exclude analytic "
+                             "templates.")
+    parser.add_argument('--obssystem-id', dest='obssys_id',
+                        type=int, default=None,
+                        help="Grab templates from a specific observing system. "
+                             "NOTE: the argument should be the obssystem_id "
+                             "from the database. "
+                             "(Default: No constraint on obssystem_id.)")
+    parser.add_argument('--obssystem-name', dest='obssystem_name',
+                        type=int, default=None,
+                        help="Grab templates from a specific observing system. "
+                             "NOTE: the argument should be the name of the "
+                             "observing system as recorded in the database. "
+                             "NOTE: SQL regular expression syntax may be used "
+                             "(Default: No constraint on obs system's name.)")
+    parser.add_argument('-t', '--telescope', dest='telescope',
+                        type=str, default=None,
+                        help="Grab templates from specific telescopes. "
+                             "The telescope's _name_ must be used here. "
+                             "NOTE: SQL regular expression syntax may be used "
+                             "(Default: No constraint on telescope name.)")
+    parser.add_argument('-f', '--frontend', dest='frontend',
+                        type=str, default=None,
+                        help="Grab templates from specific frontends. "
+                             "NOTE: SQL regular expression syntax may be used "
+                             "(Default: No constraint on frontend name.)")
+    parser.add_argument('-b', '--backend', dest='backend',
+                        type=str, default=None,
+                        help="Grab templates from specific backends. "
+                             "NOTE: SQL regular expression syntax may be used "
+                             "(Default: No constraint on backend name.)")
+    parser.add_argument('-c', '--clock', dest='clock',
+                        type=str, default=None,
+                        help="Grab templates from specific clocks. "
+                             "NOTE: SQL regular expression syntax may be used "
+                             "(Default: No constraint on clock name.)")
 
 
 def main(args):
@@ -108,7 +104,7 @@ def get_templates(args):
 
     whereclause = db.pulsar_aliases.c.pulsar_alias.like(args.pulsar_name)
     if args.template_id is not None:
-        whereclause &= (db.templates.c.template_id==args.template_id)
+        whereclause &= (db.templates.c.template_id == args.template_id)
     if args.start_date is not None:
         whereclause &= (db.templates.c.add_time >= args.start_date)
     if args.end_date is not None:
@@ -116,12 +112,12 @@ def get_templates(args):
     if args.ids:
         whereclause &= (db.templates.c.template_id.in_(args.ids))
     if args.obssys_id:
-        whereclause &= (db.obssystems.c.obssystem_id==args.obssys_id)
+        whereclause &= (db.obssystems.c.obssystem_id == args.obssys_id)
     if args.obssystem_name:
         whereclause &= (db.obssystems.c.name.like(args.obssystem_name))
     if args.telescope:
         whereclause &= (db.telescope_aliases.c.telescope_alias.\
-                                like(args.telescope))
+                        like(args.telescope))
     if args.frontend:
         whereclause &= (db.obssystems.c.frontend.like(args.frontend))
     if args.backend:
@@ -129,45 +125,45 @@ def get_templates(args):
     if args.clock:
         whereclause &= (db.obssystems.c.clock.like(args.clock))
     if args.nbin:
-        whereclause &= (db.templates.c.nbin==args.nbin)
+        whereclause &= (db.templates.c.nbin == args.nbin)
 
-    select = db.select([db.templates.c.template_id, \
-                        db.templates.c.add_time, \
-                        db.templates.c.filename, \
-                        db.templates.c.filepath, \
-                        db.templates.c.nbin, \
-                        db.templates.c.comments, \
-                        db.master_templates.c.template_id.label('mtempid'), \
-                        db.users.c.real_name, \
-                        db.users.c.email_address, \
-                        db.pulsars.c.pulsar_name, \
-                        db.telescopes.c.telescope_name, \
-                        db.obssystems.c.obssystem_id, \
-                        db.obssystems.c.name.label('obssys_name'), \
-                        db.obssystems.c.frontend, \
-                        db.obssystems.c.backend, \
-                        db.obssystems.c.clock], \
+    select = db.select([db.templates.c.template_id,
+                        db.templates.c.add_time,
+                        db.templates.c.filename,
+                        db.templates.c.filepath,
+                        db.templates.c.nbin,
+                        db.templates.c.comments,
+                        db.master_templates.c.template_id.label('mtempid'),
+                        db.users.c.real_name,
+                        db.users.c.email_address,
+                        db.pulsars.c.pulsar_name,
+                        db.telescopes.c.telescope_name,
+                        db.obssystems.c.obssystem_id,
+                        db.obssystems.c.name.label('obssys_name'),
+                        db.obssystems.c.frontend,
+                        db.obssystems.c.backend,
+                        db.obssystems.c.clock],
                 from_obj=[db.templates.\
-                    join(db.pulsar_aliases, \
-                        onclause=db.templates.c.pulsar_id == \
+                    join(db.pulsar_aliases,
+                        onclause=db.templates.c.pulsar_id ==
                                 db.pulsar_aliases.c.pulsar_id).\
-                    outerjoin(db.pulsars, \
-                        onclause=db.pulsar_aliases.c.pulsar_id == \
+                    outerjoin(db.pulsars,
+                        onclause=db.pulsar_aliases.c.pulsar_id ==
                                 db.pulsars.c.pulsar_id).\
-                    outerjoin(db.obssystems, \
-                        onclause=db.templates.c.obssystem_id == \
+                    outerjoin(db.obssystems,
+                        onclause=db.templates.c.obssystem_id ==
                                 db.obssystems.c.obssystem_id).\
-                    outerjoin(db.master_templates, \
-                        onclause=db.master_templates.c.template_id == \
+                    outerjoin(db.master_templates,
+                        onclause=db.master_templates.c.template_id ==
                                 db.templates.c.template_id).\
-                    outerjoin(db.telescopes, \
-                        onclause=db.telescopes.c.telescope_id == \
+                    outerjoin(db.telescopes,
+                        onclause=db.telescopes.c.telescope_id ==
                                 db.obssystems.c.telescope_id).\
-                    outerjoin(db.users, \
-                        onclause=db.users.c.user_id == \
+                    outerjoin(db.users,
+                        onclause=db.users.c.user_id ==
                                 db.templates.c.user_id).\
-                    join(db.telescope_aliases, \
-                        onclause=db.telescopes.c.telescope_id == \
+                    join(db.telescope_aliases,
+                        onclause=db.telescopes.c.telescope_id ==
                                 db.telescope_aliases.c.telescope_id)],
                 distinct=db.templates.c.template_id).\
                 where(whereclause)
@@ -183,27 +179,27 @@ def show_templates(templates):
         print "--"*25
         for tdict in templates:
             print colour.cstring("Template ID:", underline=True, bold=True) + \
-                    colour.cstring(" %d" % tdict['template_id'], bold=True)
+                colour.cstring(" %d" % tdict['template_id'], bold=True)
             fn = os.path.join(tdict['filepath'], tdict['filename'])
             print "\nTemplate: %s" % fn
             print "Pulsar name: %s" % tdict['pulsar_name']
             print "Master template? %s" % \
-                    (((tdict['mtempid'] is not None) and "Yes") or "No")
+                  (((tdict['mtempid'] is not None) and "Yes") or "No")
             print "Number of phase bins: %d" % tdict['nbin']
-            print "Uploaded by: %s (%s)" % (tdict['real_name'], \
+            print "Uploaded by: %s (%s)" % (tdict['real_name'],
                                             tdict['email_address'])
             print "Uploader's comments: %s" % tdict['comments']
             print "Date and time template was added: %s" % \
-                                tdict['add_time'].isoformat(' ')
+                  tdict['add_time'].isoformat(' ')
 
             # Show extra information if verbosity is >= 1
-            lines = ["Observing System ID: %d" % tdict['obssystem_id'], \
-                     "Observing System Name: %s" % tdict['obssys_name'], \
-                     "Telescope: %s" % tdict['telescope_name'], \
-                     "Frontend: %s" % tdict['frontend'], \
-                     "Backend: %s" % tdict['backend'], \
+            lines = ["Observing System ID: %d" % tdict['obssystem_id'],
+                     "Observing System Name: %s" % tdict['obssys_name'],
+                     "Telescope: %s" % tdict['telescope_name'],
+                     "Frontend: %s" % tdict['frontend'],
+                     "Backend: %s" % tdict['backend'],
                      "Clock: %s" % tdict['clock']]
-            utils.print_info("\n".join(lines), 1)
+            notify.print_info("\n".join(lines), 1)
            
             try:
                 # Show the template if verbosity is >= 2
@@ -223,9 +219,10 @@ def show_templates(templates):
                                  %s
                                  end
                             """ % (tdict.nbin-1, psrtxtout)
-                plot, stderr = utils.execute(["gnuplot"], \
-                                stderr=open(os.devnull), stdinstr=gnuplotcode)
-                utils.print_info(plot, 2)
+                plot, stderr = utils.execute(["gnuplot"],
+                                             stderr=open(os.devnull),
+                                             stdinstr=gnuplotcode)
+                notify.print_info(plot, 2)
             except errors.SystemCallError:
                 # gnuplot is probably not installed
                 pass
@@ -234,7 +231,7 @@ def show_templates(templates):
         raise errors.ToasterError("No templates match parameters provided!")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = utils.DefaultArguments(description=DESCRIPTION)
     add_arguments(parser)
     args = parser.parse_args()
