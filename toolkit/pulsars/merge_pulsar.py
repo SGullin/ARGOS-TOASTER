6fdd0e6a33fa855661c5e1918da297ad874b9d55
Patrick Lazarus <plazarus@mpifr.de>
2014-02-20 15:04:21 +0100

    Fix imports and formatting in 'merge_pulsar.py'.

--- toolkit/pulsars/merge_pulsar.py
+++ toolkit/pulsars/merge_pulsar.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import utils
-import errors
-import database
+from toaster import utils
+from toaster import errors
+from toaster import database
+from toaster.utils import cache
+from toaster.utils import notify
 
 SHORTNAME = 'merge'
 DESCRIPTION = "Merge a pulsar entry in the database into another entry. " \
@@ -12,11 +14,11 @@ DESCRIPTION = "Merge a pulsar entry in the database into another entry. " \
 
 
 def add_arguments(parser):
-    parser.add_argument('-p', '--psr', dest='src_psrname', type=str, \
-                        help="The pulsar to merge. NOTE: All instances " \
-                            "of this pulsar's corresponding ID number " \
-                            "will be changed in the database.")
-    parser.add_argument('--into', dest='dest_psrname', type=str, \
+    parser.add_argument('-p', '--psr', dest='src_psrname', type=str,
+                        help="The pulsar to merge. NOTE: All instances "
+                             "of this pulsar's corresponding ID number "
+                             "will be changed in the database.")
+    parser.add_argument('--into', dest='dest_psrname', type=str,
                         help="The pulsar entry to be merged into.")
 
 
@@ -37,22 +39,22 @@ def merge_pulsar(src_pulsar_id, dest_pulsar_id, existdb=None):
         Outputs:
             None
     """
-    utils.print_info("Merging pulsar '%s' (ID: %d) into '%s' (ID: %d)" % \
-                    (utils.get_pulsarname(src_pulsar_id), src_pulsar_id, \
-                     utils.get_pulsarname(dest_pulsar_id), dest_pulsar_id), 2)
+    notify.print_info("Merging pulsar '%s' (ID: %d) into '%s' (ID: %d)" %
+                    (cache.get_pulsarname(src_pulsar_id), src_pulsar_id,
+                     cache.get_pulsarname(dest_pulsar_id), dest_pulsar_id), 2)
     # Connect to the database
     db = existdb or database.Database()
     db.connect()
     trans = db.begin()
     try:
         # Update all relevant entries in the database
-        tables = [db.pulsar_aliases, \
-                  db.timfiles, \
-                  db.rawfiles, \
-                  db.templates, \
-                  db.parfiles, \
-                  db.master_parfiles, \
-                  db.master_templates, \
+        tables = [db.pulsar_aliases,
+                  db.timfiles,
+                  db.rawfiles,
+                  db.templates,
+                  db.parfiles,
+                  db.master_parfiles,
+                  db.master_templates,
                   db.toas]
         values = {'pulsar_id': dest_pulsar_id}
         for table in tables:
@@ -77,16 +79,17 @@ def merge_pulsar(src_pulsar_id, dest_pulsar_id, existdb=None):
 
 
 def main(args):
-    src_pulsar_id = utils.get_pulsarid(args.src_psrname)
-    dest_pulsar_id = utils.get_pulsarid(args.dest_psrname)
+    src_pulsar_id = cache.get_pulsarid(args.src_psrname)
+    dest_pulsar_id = cache.get_pulsarid(args.dest_psrname)
     if src_pulsar_id == dest_pulsar_id:
-        raise errors.BadInputError("Cannot merge '%s' (ID: %d) into " \
-                    "itself ('%s')" % \
-                    (args.src_psrname, src_pulsar_id, args.dest_psrname))
+        raise errors.BadInputError("Cannot merge '%s' (ID: %d) into "
+                                   "itself ('%s')" %
+                                   (args.src_psrname, src_pulsar_id,
+                                    args.dest_psrname))
     merge_pulsar(src_pulsar_id, dest_pulsar_id)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = utils.DefaultArguments(description=DESCRIPTION)
     add_arguments(parser)
     args = parser.parse_args()
