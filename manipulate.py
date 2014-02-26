64ac3bffd279063322411b9840753a1360f545bb
Patrick Lazarus <plazarus@mpifr.de>
2014-02-26 15:01:43 +0100

    Fix formatting and imports in 'manipulate.py'.

--- manipulate.py
+++ manipulate.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-import sys
-import argparse
 
-import manipulators
+from toaster import manipulators
+
 
 def main():
     manip = manipulators.load_manipulator(args.manip_name)
@@ -10,18 +9,18 @@ def main():
     manip.run(args.infiles, outname=args.outfile)
 
 
-if __name__=='__main__':
-    parser = manipulators.ManipulatorArguments(\
-                            description="Call a manipulator plugin's " \
-                                        "manipulate method with the given " \
+if __name__ == '__main__':
+    parser = manipulators.ManipulatorArguments(
+                            description="Call a manipulator plugin's "
+                                        "manipulate method with the given "
                                         "arguments on the given files.")
-    parser.add_argument('infiles', metavar="INFILES", \
-                         nargs='+', type=str, \
-                         help="Archive files to be manipulated.")
-    parser.add_argument('-o', '--outfile', type=str, dest='outfile', \
-                       help="Name of file to output the resulting "
-                             "manipulated archive to. (Default: " \
-                             "don't write output file to disk.)", \
-                         default=None)
+    parser.add_argument('infiles', metavar="INFILES",
+                        nargs='+', type=str,
+                        help="Archive files to be manipulated.")
+    parser.add_argument('-o', '--outfile', type=str, dest='outfile',
+                        help="Name of file to output the resulting "
+                             "manipulated archive to. (Default: "
+                             "don't write output file to disk.)",
+                        default=None)
     args, leftover_args = parser.parse_known_args()
     main()
