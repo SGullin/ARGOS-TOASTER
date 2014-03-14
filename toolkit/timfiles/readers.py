3710f91b4de3aa2cf8e21d2cf6ac8924f97be41b
Patrick Lazarus <plazarus@mpifr.de>
2014-03-14 15:28:21 +0100

    Fix imports and formatting in 'readers.py'.

--- toolkit/timfiles/readers.py
+++ toolkit/timfiles/readers.py
@@ -5,20 +5,21 @@ Patrick Lazarus, Apr. 2, 2013
 """
 import re
 
-import utils
-import errors
+from toaster.utils import notify
+from toaster.utils import cache
 
 # The following dictionary specifies the data type for
 # expected flags. All keys should be lower case.
-KNOWN_FLAG_TYPES = {'bw': float, \
-                    'length': float, \
-                    'nbin': int, \
+KNOWN_FLAG_TYPES = {'bw': float,
+                    'length': float,
+                    'nbin': int,
                     'goodness_of_fit': float}
 
 # The following dictionary specifies alternative names for
 # expected flags. All keys should be lower case.
-KNOWN_FLAG_ALIASES = {'bandwidth': 'bw', \
+KNOWN_FLAG_ALIASES = {'bandwidth': 'bw',
                       'gof': 'goodness_of_fit'}
+
 
 def tempo2_reader(line):
     """Parse line, assuming it is a TOA in tempo2 format.
@@ -30,16 +31,16 @@ def tempo2_reader(line):
         Output:
             toainfo: A dictionary of TOA information.
     """
-    tempo2_toa_re = re.compile(r'^ *(?P<bad>(#|(C ))(?P<comment1>.*?))? *' \
-                               r'(?P<file>[^ ]+) +' \
-                               r'(?P<freq>\d+(\.\d+)?) +(?P<imjd>\d+)(?P<fmjd>\.\d+) +' \
+    tempo2_toa_re = re.compile(r'^ *(?P<bad>(#|(C ))(?P<comment1>.*?))? *'
+                               r'(?P<file>[^ ]+) +'
+                               r'(?P<freq>\d+(\.\d+)?) +(?P<imjd>\d+)(?P<fmjd>\.\d+) +'
                                r'(?P<err>\d+(\.\d+)?) +(?P<site>[^ ]+)')
     comment_re = re.compile(r'#(?P<comment>.*)$')
     tempo2_flag_re = re.compile(r'-(?P<flagkey>[^ ]+) +(?P<flagval>[^ ]+)')
     match = tempo2_toa_re.search(line)
     if match is None:
         toainfo = None
-        utils.print_debug("Line is not a Tempo2 TOA:\n    %s" % line, 'toaparse')
+        notify.print_debug("Line is not a Tempo2 TOA:\n    %s" % line, 'toaparse')
     else:
         grp = match.groupdict()
      
@@ -51,7 +52,7 @@ def tempo2_reader(line):
         toainfo['fmjd'] = float(grp['fmjd'])
         toainfo['toa_unc_us'] = float(grp['err'])
         toainfo['telescope'] = grp['site']
-        toainfo['telescope_id'] = utils.get_telescope_info(grp['site'])['telescope_id']
+        toainfo['telescope_id'] = cache.get_telescope_info(grp['site'])['telescope_id']
         comments = []
         if grp['comment1']:
             comments.append(grp['comment1'].strip())
@@ -70,9 +71,9 @@ def tempo2_reader(line):
             try:
                 toainfo['extras'][key] = caster(val)
             except:
-                utils.print_info("Couldn't cast %s:%s" % (key, val), 2)
+                notify.print_info("Couldn't cast %s:%s" % (key, val), 2)
 
-    utils.print_debug("TOA line: %s\nParsed info: %s" % (line, toainfo), \
+    notify.print_debug("TOA line: %s\nParsed info: %s" % (line, toainfo),
                     'toaparse')
 
     return toainfo
@@ -88,16 +89,16 @@ def parkes_reader(line):
         Output:
             toainfo: A dictionary of TOA information.
     """
-    parkes_toa_re = re.compile(r'^ *?(?P<bad>(#|(C ))(?P<comment1>.*?))?' \
-                               r' (?P<info>.{24})(?P<freq>.{9})(?P<imjd>.{7})(?P<fmjd>\..{12}) ' \
-                               r'(?P<phaseoffset>.{8}) (?P<err>.{7})(?P<info2>.{7}) '\
+    parkes_toa_re = re.compile(r'^ *?(?P<bad>(#|(C ))(?P<comment1>.*?))?'
+                               r' (?P<info>.{24})(?P<freq>.{9})(?P<imjd>.{7})(?P<fmjd>\..{12}) '
+                               r'(?P<phaseoffset>.{8}) (?P<err>.{7})(?P<info2>.{7}) '
                                r'(?P<site>.)(?P<dmcorr>[^#]*)')
     comment_re = re.compile(r'#(?P<comment>.*)$')
     
     match = parkes_toa_re.search(line.rstrip())
     if match is None:
         toainfo = None
-        utils.print_debug("Line is not a Parkes-format TOA:\n    %s" % line, 'toaparse')
+        notify.print_debug("Line is not a Parkes-format TOA:\n    %s" % line, 'toaparse')
     else:
         grp = match.groupdict()
         toainfo = {}
@@ -107,8 +108,8 @@ def parkes_reader(line):
         toainfo['fmjd'] = float(grp['fmjd'])
         toainfo['toa_unc_us'] = float(grp['err'])
         toainfo['telescope'] = grp['site']
-        toainfo['telescope_id'] = utils.get_telescope_info(grp['site'])['telescope_id']
-        toainfo['extras'] = {'phaseoffset': float(grp['phaseoffset']), \
+        toainfo['telescope_id'] = cache.get_telescope_info(grp['site'])['telescope_id']
+        toainfo['extras'] = {'phaseoffset': float(grp['phaseoffset']),
                              'infostr': grp['info'].strip() + ' -- ' + grp['info2'].strip()}
         if grp['dmcorr']:
             toainfo['extras']['dmcorr'] = float(grp['dmcorr'])
